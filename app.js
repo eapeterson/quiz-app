485efe910481f44ff78d69a0e7b1fd42cec84160
eapeterson <elizabeth.peterson1229@gmail.com>
2015-09-23 09:03:56 -0400

    Second commit

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
 $(document).ready(function(){
 
+	//var image = $("#graphic")
+
+	//image.html(span)
+
+	//$(".answer")
+
+	$(".tardis").css('opacity', 0.5);//change with every question correct
+
+	var hq { 
+		q: ""//question asked
+		o: [//array of answers]
+		c: 0 //correct answer
+		s: 'url' //image url
+	}
+
+	$("#question").text(hq[0].q);
+	$("#opt0" + x).next('span').text(hq[0].o[0]);
+
+
 
 
 
