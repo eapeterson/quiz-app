59dd33812ba403bc8abaa8a7768e560f3f321896
eapeterson <elizabeth.peterson1229@gmail.com>
2015-09-29 14:49:35 -0400

    Fifth commit

--- app.js
+++ app.js
@@ -25,42 +25,48 @@ $(document).ready(function(){
         question: "Question #1 of 5: Who is this character?",
         answers: ["The Master", "Rose Tyler", "Clara Oswald", "River Song"],
         correctAnswer : 3,
-        picture: "/quiz-app/images/River_Looks_Left.jpg"//'http://i.imgur.com/tIl2Gsst.jpg'
+        picture: "images/River_Looks_Left.jpg"//'http://i.imgur.com/tIl2Gsst.jpg'
         },
 
         {
         question: "Question #2 of 5: What does TARDIS stand for?",
         answers: ["The Actual Returning Device In Skaro", "Time And Relative Dimension In Space", "Time Around Running Distance In Space", "Traveling And Returning Device In Space"],
         correctAnswer : 1,
-        picture: "/quiz-app/images/tardis.jpg"
+        picture: "images/tardis.jpg"
         },
 
         {
         question: "Question #3 of 5: What planet is the Doctor from?",
         answers: ["Second Earth", "Cheem", "Gallifrey", "Skaro"],
         correctAnswer : 2,
-        picture: "../images/pandorica.jpg"
+        picture: "images/pandorica.jpg"
         },
 
         {
         question: "Question #4 of 5: Which of these people NEVER had a crush on the Doctor?",
         answers: ["Donna Noble", "Amy Pond", "Martha Jones", "Jack Harkness"],
         correctAnswer : 0,
-        picture: "../images/heartbeat.jpg"
+        picture: "images/heartbeat.jpg"
         },
 
         {
         question: "Question #5 of 5: Which character protected someone for 2000 years?",
         answers: ["Amy Pond", "The Face of Bo", "Danny Pink", "Rory Williams"],
         correctAnswer : 3,
-        picture: "../images/pandorica.jpg"
+        picture: "images/pandorica.jpg"
     }];
 
 	console.log(heroQuestions[3]);
 
+	function moveQuestion () {
+		$("#questionBox").next('span').text(heroQuestions[0].answers[0].picture[0]);
+
+	}
+
 	$('#question').html(heroQuestions[0]);
 
 	$('#submitAnswer').on("click", function () {
+		moveQuestion();
 		tardisFade();
 		currentQuestion++;
 		//$('#question').html(heroQuestions.next())
